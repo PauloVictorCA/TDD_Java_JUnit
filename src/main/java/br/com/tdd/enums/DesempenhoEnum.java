9539ca1cd3a01e3826c027385cdf45a489db5ac9
PauloVictorCA <paulovictor.ca@outlook.com>
2023-06-06 21:05:05 -0300

    Refatoração do ReajusteService

--- src/main/java/br/com/tdd/enums/DesempenhoEnum.java
+++ src/main/java/br/com/tdd/enums/DesempenhoEnum.java
@@ -1,9 +1,28 @@
 package br.com.tdd.enums;
 
+import java.math.BigDecimal;
+
 public enum DesempenhoEnum {
 
-	A_DESEJAR,
-	BOM,
-	OTIMO;
+	A_DESEJAR {
+		@Override
+		public BigDecimal percentualReajuste() {
+			return new BigDecimal("0.03");
+		}
+	},
+	BOM {
+		@Override
+		public BigDecimal percentualReajuste() {
+			return new BigDecimal("0.15");
+		}
+	},
+	OTIMO {
+		@Override
+		public BigDecimal percentualReajuste() {
+			return new BigDecimal("0.20");
+		}
+	};
+	
+	public abstract BigDecimal percentualReajuste();
 	
 }
